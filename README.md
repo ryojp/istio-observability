c97ac18021bf10bd8f30450f697ac027db10e499
Ryo Koike <ryok022366@gmail.com>
2023-05-21 11:02:56 +0900

    delete Jaeger istrumentation as Istio provides it

--- README.md
+++ README.md
@@ -23,8 +23,7 @@ docker run -d -p 5000:5000 --restart always --name registry registry:2
 
 1. Build and push `fastapi_app` image to `localhost:5000`:
 ```sh
-docker build -t localhost:5000/fastapi_app fastapi_app/
-
+docker build -t localhost:5000/fastapi_app fastapi_app/ &&\
 docker push localhost:5000/fastapi_app
 ```
 
@@ -40,7 +39,7 @@ kubectl apply -f k8s
 
 4. Issue requests to the FastAPI app:
 ```sh
-curl http://localhost/chain
+curl localhost/chain
 ```
 
 5. Open Jaeger dashboard:
@@ -52,3 +51,40 @@ istioctl dashboard jaeger
 
 Somehow Jaeger UI does not correlate requests that (should have) the same correlation ID.
 Now, they are displayed as individual requests, such as those between `app-a` and `app-b`, `app-a` and `app-c`, etc.
+
+
+## Troubleshooting
+
+### `curl localhost/chain` returns `404 Not Found` HTML from NGINX
+
+In this case, you are likely to have `NGINX Ingress Controller` deployed on your systme, and it served the `curl` request instead of the `Istio Ingress Gateway`.  
+Please delete your ingress and run again.
+
+For example, if you are using `microk8s`, you can run `microk8s disable ingress`
+
+### `curl localhost` hangs
+
+Make sure that you can connect to the upstream webp app directly using port-forward:
+```sh
+# On one terminal window
+k -n demo port-forward app-a-deployment-b75df8b88-46gqc 8080:80 # Please change the pod name appropriately
+# On another terminal window
+curl localhost:8080
+```
+
+If this returns a response, the problem is likely to be on the Ingress Gateway of Istio.
+
+For some environments including `microk8s` on Ubuntu (in my case, Xubuntu 22.04), `LoadBalancer` does not seem to work.
+As the [official doc](https://istio.io/latest/docs/tasks/traffic-management/ingress/ingress-control/#using-node-ports-of-the-ingress-gateway-service) mentions, change the `spec.type` of `ingress-gateway` from `LoadBalancer` to `NodePort` to fix this.
+```sh
+kubectl -n istio-system edit svc istio-ingressgateway
+```
+
+Next, get the port number that maps to `80` by:
+```sh
+$ k -n istio-system get svc istio-ingressgateway
+NAME                   TYPE       CLUSTER-IP       EXTERNAL-IP   PORT(S)                                                                      AGE
+istio-ingressgateway   NodePort   10.152.183.198   <none>        15021:32569/TCP,80:30419/TCP,443:32250/TCP,31400:30769/TCP,15443:32302/TCP   66m
+```
+
+In this case, `localhost:30419` is what you want. Try `curl localhost:30419` to verify it.
--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -7,80 +7,13 @@ from typing import Optional
 import httpx
 import uvicorn
 from fastapi import FastAPI, Response, Request
-from opentelemetry import trace
-from opentelemetry.exporter.jaeger.proto.grpc import \
-    JaegerExporter as GrpcJaegerExporter
-from opentelemetry.exporter.jaeger.thrift import \
-    JaegerExporter as ThriftJaegerExporter
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry.propagate import inject
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from starlette.types import ASGIApp
 
-JAEGER_HOST = 'jaeger-collector.istio-system.svc.cluster.local'
-
-APP_NAME = os.environ.get("APP_NAME", "app")
-EXPOSE_PORT = int(os.environ.get("PORT", "8000"))
-
-# grpc, thrift-collector,  thrift-agent
-MODE = os.environ.get("MODE", "grpc")
-
-# with grpc through jaeger collector
-COLLECTOR_ENDPOINT_GRPC_ENDPOINT = os.environ.get(
-    "COLLECTOR_ENDPOINT_GRPC_ENDPOINT", f"{JAEGER_HOST}:14250")
-
-# with thrift through jaeger collector
-COLLECTOR_THRIFT_URL = os.environ.get(
-    "COLLECTOR_THRIFT_URL", f"http://{JAEGER_HOST}:14268")
-
-# with thrift through jaeger agent
-AGENT_HOST_NAME = os.environ.get("AGENT_HOST_NAME", "jaeger-agent")
-AGENT_PORT = int(os.environ.get("AGENT_PORT", 6831))
-
-TARGET_ONE_HOST = os.environ.get("TARGET_ONE_HOST", "app-b.demo.svc.cluster.local")
-TARGET_TWO_HOST = os.environ.get("TARGET_TWO_HOST", "app-c.demo.svc.cluster.local")
+PORT = int(os.environ.get("PORT", "8000"))
+TARGET_ONE_ORIGIN = os.environ.get("TARGET_ONE_ORIGIN", "http://app-b.demo.svc.cluster.local")
+TARGET_TWO_ORIGIN = os.environ.get("TARGET_TWO_ORIGIN", "http://app-c.demo.svc.cluster.local")
 
 app = FastAPI()
 
-
-def setting_jaeger(app: ASGIApp, app_name: str, log_correlation: bool = True) -> None:
-    # Setting jaeger
-    # set the service name to show in traces
-    resource = Resource.create(attributes={
-        "service.name": app_name
-    })
-
-    # set the tracer provider
-    tracer = TracerProvider(resource=resource)
-    trace.set_tracer_provider(tracer)
-
-    # use different mode to push span according environment variable MODE
-    if MODE == "thrift-collector":
-        tracer.add_span_processor(BatchSpanProcessor(ThriftJaegerExporter(
-            collector_endpoint=f'{COLLECTOR_THRIFT_URL}/api/traces?format=jaeger.thrift',
-        )))
-    elif MODE == "thrift-agent":
-        tracer.add_span_processor(BatchSpanProcessor(ThriftJaegerExporter(
-            agent_host_name=AGENT_HOST_NAME,
-            agent_port=AGENT_PORT,
-        )))
-    else:
-        # grpc (default)
-        tracer.add_span_processor(BatchSpanProcessor(GrpcJaegerExporter(
-            collector_endpoint=COLLECTOR_ENDPOINT_GRPC_ENDPOINT, insecure=True)))
-
-    # override logger format which with trace id and span id
-    if log_correlation:
-        LoggingInstrumentor().instrument(set_logging_format=True)
-
-    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
-
-
-# Setting jaeger exporter
-setting_jaeger(app, APP_NAME)
 
 # logger middleware
 @app.middleware("http")
@@ -126,9 +59,16 @@ async def random_status(response: Response):
 
 @app.get("/random_sleep")
 async def random_sleep(response: Response):
-    time.sleep(random.randint(0, 5))
     logging.debug("random sleep")
+    time.sleep(random.randint(0, 5))
     return {"path": "/random_sleep"}
+
+
+@app.get("/sleep")
+async def random_sleep(response: Response, t: Optional[float] = 1.0):
+    logging.debug(f"sleeping {t} sec...")
+    time.sleep(t)
+    return {"path": "/sleep", "t": t}
 
 
 @app.get("/error_test")
@@ -140,21 +80,15 @@ async def random_sleep(response: Response):
 @app.get("/chain")
 async def chain(response: Response):
 
-    headers = {}
-    inject(headers)  # inject trace info to header
-    logging.info(headers)
-
     async with httpx.AsyncClient() as client:
-        await client.get(f"http://localhost:8000/", headers=headers,)
+        await client.get(f"http://localhost:{PORT}/")
     async with httpx.AsyncClient() as client:
-        await client.get(f"http://{TARGET_ONE_HOST}:8000/io_task", headers=headers,)
+        await client.get(f"{TARGET_ONE_ORIGIN}/io_task")
     async with httpx.AsyncClient() as client:
-        await client.get(f"http://{TARGET_TWO_HOST}:8000/cpu_task", headers=headers,)
+        await client.get(f"{TARGET_TWO_ORIGIN}/cpu_task")
     logging.info("Chain Finished")
     return {"path": "/chain"}
 
 if __name__ == "__main__":
-    # update uvicorn access logger format
-    log_config = uvicorn.config.LOGGING_CONFIG
-    log_config["formatters"]["access"]["fmt"] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
-    uvicorn.run(app, host="0.0.0.0", port=EXPOSE_PORT, log_config=log_config)
+    logging.info(f"Running on port {PORT}")
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
